a736762448a74d8b26ee1bc496a4980dc812967d
Magnus93 <magnus.ja.gustafsson@gmail.com>
2022-04-24 22:01:18 +0200

    Add filter

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	export let name: string;
-	let text;
+	let csv: string;
   let transactions: Record<string, any>[];
+  let filteredTransactions: Record<string, any>[];
   let headers: string[];
 
   function handleFileSelected(event: any) {
@@ -10,8 +11,9 @@
     const reader = new FileReader();
     reader.onload = (e) => {
       e.target.result
-      text = e.target.result;
-      transactions = csvToObject(text, ";");
+      csv = e.target.result as string;
+      transactions = csvToObject(csv, ";");
+      filteredTransactions = transactions
     }
     reader.readAsText(files[0]);
   }
@@ -27,12 +29,19 @@
     return result
   }
   function convert(value: string): string | number {
-    let num: undefined | number
     return value == "" 
     ? 0 
     : (/^\-?\d+(\.\d+)?(kr)?$/g.test(value.replace(",", ".").replace(/ /g, "").replace("\r", "")) 
     ? +value.substring(0, value.length-4).replace(",", ".").replace(/ /g, "").replace("\r", "") 
     : value)
+  }
+
+  function handleFilterEvent(e: Event) {
+    console.log(e.target)
+    const property = (e.target as HTMLInputElement)?.getAttribute("name")
+    const search = (e.target as HTMLInputElement).value
+    console.log(search)
+    filteredTransactions = transactions.filter(t => RegExp(search, "i").test(t[property]))
   }
 </script>
 
@@ -48,15 +57,21 @@
   <div>
     <h2>CSV</h2>
     <h2>Object</h2>
-    <!-- <pre>{text}</pre> -->
     <table>
       <thead>
+        <tr>
+          {#each headers ?? [] as h}
+            <th>
+              <input type="text" name={h} on:input={handleFilterEvent} />
+            </th>
+          {/each}
+        </tr>
         <tr>
           {#each headers ?? [] as h}
             <th>{h}</th>
           {/each}
         </tr>
-        {#each transactions ?? [] as o}
+        {#each filteredTransactions ?? [] as o}
           <tr>
             {#each headers ?? [] as h}
             <td>{o[h]}</td>
@@ -80,7 +95,9 @@
     display: grid;
     grid-template-columns: 3fr 1fr;
   }
-
+  input[type="text"] {
+    max-width: 10rem;
+  }
   table {
     height: min-content;
   }
@@ -93,12 +110,16 @@
 
 	h1 {
 		color: #ff3e00;
-		font-size: 3em;
+		font-size: 2rem;
 		font-weight: 200;
-    margin: 1rem 0;
+    margin: 0.5rem 0;
 	}
   h1 > i {
     font-size: 2rem;
+  }
+  :global(body) {
+    margin: 0;
+    padding: 0;
   }
 
 	@media (min-width: 640px) {
