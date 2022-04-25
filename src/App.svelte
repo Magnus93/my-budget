278fe00cb331db1747cdb6a731a59f3ca653978b
Magnus93 <magnus.ja.gustafsson@gmail.com>
2022-04-25 20:41:59 +0200

    Moved upload to own function

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import Edit from "./Edit.svelte";
+  import Upload from "./Upload.svelte";
 	export let name: string;
 	let csv: string;
   let transactions: Record<string, any>[];
@@ -12,48 +13,15 @@
     {name: "furniture", color: "purple"},
     {name: "misc", color: "teal"},
   ]
-
-  function handleFileSelected(event: any) {
-    const files = event.target.files
-    console.log(files);
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      e.target.result
-      csv = e.target.result as string;
-      transactions = csvToObject(csv, ";");
-      filteredTransactions = transactions
-    }
-    reader.readAsText(files[0]);
-  }
-
-  function csvToObject(csv: string, divider=","): Record<string, any>[] {
-    let result = []
-    let splitted = csv.split("\n")
-    headers = splitted[0].split(divider).map(h => h.replace("\r", ""))
-    splitted.splice(1).forEach((row) => {
-      let values = row.split(divider)
-      result.push(values.reduce((r, c, i) => ({...r, [headers[i]]: convert(c.trim())}), {}))
-    })
-    return result
-  }
-  function convert(value: string): string | number {
-    return value == "" 
-    ? 0 
-    : (/^\-?\d+(\.\d+)?(kr)?$/g.test(value.replace(",", ".").replace(/ /g, "").replace("\r", "")) 
-    ? +value.substring(0, value.length-4).replace(",", ".").replace(/ /g, "").replace("\r", "") 
-    : value)
+  function uploadHander(event: CustomEvent<{transactions: Record<string, any>[], headers: string[]}>) {
+    transactions = event.detail.transactions
+    headers = event.detail.headers
   }
 </script>
 
 <main>
 	<h1>{name}<i>(Magnus and Yash's Budget)</i></h1>
-  <fieldset>
-    <legend>Upload CSV</legend>
-	  <input type="file" id="budget-file" name="filename" accept=".csv" on:change={(e) => {
-	  	console.log(e);
-	  	handleFileSelected(e);
-	  }}>
-  </fieldset>
+  <Upload on:upload={uploadHander}></Upload>
   <div>
     <Edit transactions={transactions} headers={headers} categories={categories}></Edit>
 </main>
---
+++ src/Upload.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+  const dispatch = createEventDispatcher<{upload: {transactions: Record<string, any>[], headers: string[]}}>();
+  let csv: string;
+  let transactions: Record<string, any>[];
+  let headers: string[];
+
+  function handleFileSelected(event: any) {
+    const files = event.target.files;
+    console.log(files);
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      e.target.result;
+      csv = e.target.result as string;
+      transactions = csvToObject(csv, ";");
+      dispatch("upload", {transactions, headers})
+    };
+    reader.readAsText(files[0]);
+  }
+  function csvToObject(csv: string, divider=","): Record<string, any>[] {
+    let result = []
+    let splitted = csv.split("\n")
+    headers = splitted[0].split(divider).map(h => h.replace("\r", ""))
+    splitted.splice(1).forEach((row) => {
+      let values = row.split(divider)
+      result.push(values.reduce((r, c, i) => ({...r, [headers[i]]: convert(c.trim())}), {}))
+    })
+    return result
+  }
+  function convert(value: string): string | number {
+    return value == "" 
+    ? 0 
+    : (/^\-?\d+(\.\d+)?(kr)?$/g.test(value.replace(",", ".").replace(/ /g, "").replace("\r", "")) 
+    ? +value.substring(0, value.length-4).replace(",", ".").replace(/ /g, "").replace("\r", "") 
+    : value)
+  }
+</script>
+
+<fieldset>
+  <legend>Upload CSV</legend>
+  <input
+    type="file"
+    id="budget-file"
+    name="filename"
+    accept=".csv"
+    on:change={(e) => {
+      console.log(e);
+      handleFileSelected(e);
+    }}
+  />
+</fieldset>
