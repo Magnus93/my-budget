b2c3902f0a32226958dedcceb09794bacdedc713
Magnus93 <magnus.ja.gustafsson@gmail.com>
2022-04-25 20:16:26 +0200

    Move edit to own component

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import Edit from "./Edit.svelte";
 	export let name: string;
 	let csv: string;
   let transactions: Record<string, any>[];
@@ -42,14 +43,6 @@
     ? +value.substring(0, value.length-4).replace(",", ".").replace(/ /g, "").replace("\r", "") 
     : value)
   }
-
-  function handleFilterEvent(e: Event) {
-    console.log(e.target)
-    const property = (e.target as HTMLInputElement)?.getAttribute("name")
-    const search = (e.target as HTMLInputElement).value
-    console.log(search)
-    filteredTransactions = transactions.filter(t => RegExp(search, "i").test(t[property]))
-  }
 </script>
 
 <main>
@@ -62,38 +55,7 @@
 	  }}>
   </fieldset>
   <div>
-    <h2>Transactions</h2>
-    <div class="categories">
-      Add selection to:
-      {#each categories as g}
-        <span class={"category " + g.color} name={g.name}>{g.name}</span>
-      {/each}
-    </div>
-    <table>
-      <thead>
-        <tr>
-          {#each headers ?? [] as h}
-            <th>
-              <input type="text" name={h} on:input={handleFilterEvent} />
-            </th>
-          {/each}
-        </tr>
-        <tr>
-          {#each headers ?? [] as h}
-            <th>{h}</th>
-          {/each}
-        </tr>
-        {#each filteredTransactions ?? [] as o}
-          <tr>
-            {#each headers ?? [] as h}
-            <td>{o[h]}</td>
-            {/each}
-          </tr>
-        {/each}
-        
-      </thead>
-    </table>
-  </div>
+    <Edit transactions={transactions} headers={headers} categories={categories}></Edit>
 </main>
 
 <style>
@@ -106,26 +68,7 @@
     display: grid;
     /* grid-template-columns: 3fr 1fr; */
   }
-  input[type="text"] {
-    width: 100%;
-  }
-  table {
-    height: min-content;
-  }
-  tr:nth-child(2n) {
-    background-color: aliceblue;
-  }
-  tr:not(:first-child):hover {
-    outline: 1px solid blue;
-  }
-  .categories {
-    padding: 1rem 0;
-  }
-  .category {
-    padding: 0.25em 0.75em;
-    margin: 0 0.5em;
-    border-radius: 0.5rem;
-  }
+
 	h1 {
 		color: #ff3e00;
 		font-size: 2rem;
@@ -138,27 +81,6 @@
   :global(body) {
     margin: 0;
     padding: 0;
-  }
-  .red, .blue, .orange, .green, .purple, .teal {
-    color: white;
-  }
-  .red {
-    background-color: #761f1f;
-  }
-  .blue {
-    background-color: #1f3076;
-  }
-  .orange {
-    background-color: #7e4f19;
-  }
-  .green {
-    background-color: #28761f;
-  }
-  .purple {
-    background-color: #6c1f76;
-  }
-  .teal {
-    background-color: #1f7276;
   }
 
 	@media (min-width: 640px) {
---
+++ src/Edit.svelte
@@ -0,0 +1,96 @@
+<script lang="ts">
+  export let headers: string[] | undefined = undefined;
+  export let categories: {name: string, color?: string}[] | undefined = undefined;
+  export let transactions: Record<string, any>[] | undefined = undefined;
+  let filteredTransactions: Record<string, any>[] | undefined;
+
+  $: {
+    filteredTransactions = transactions;
+  }
+
+  function handleFilterEvent(e: Event) {
+    console.log(e.target)
+    const property = (e.target as HTMLInputElement)?.getAttribute("name")
+    const search = (e.target as HTMLInputElement).value
+    console.log(search)
+    filteredTransactions = transactions.filter(t => RegExp(search, "i").test(t[property]))
+  }
+</script>
+
+<div>
+  {transactions?.length}
+  <h2>Edit Transactions</h2>
+  <div class="categories">
+    Add selection to:
+    {#each categories ?? [] as g}
+      <span class={"category " + g.color} name={g.name}>{g.name}</span>
+    {/each}
+  </div>
+  <table>
+    <thead>
+      <tr>
+        {#each headers ?? [] as h}
+          <th>
+            <input type="text" name={h} on:input={handleFilterEvent} />
+          </th>
+        {/each}
+      </tr>
+      <tr>
+        {#each headers ?? [] as h}
+          <th>{h}</th>
+        {/each}
+      </tr>
+      {#each filteredTransactions ?? [] as o}
+        <tr>
+          {#each headers ?? [] as h}
+            <td>{o[h]}</td>
+          {/each}
+        </tr>
+      {/each}
+    </thead>
+  </table>
+</div>
+
+<style>
+  input[type="text"] {
+    width: 100%;
+  }
+  table {
+    height: min-content;
+  }
+  tr:nth-child(2n) {
+    background-color: aliceblue;
+  }
+  tr:not(:first-child):hover {
+    outline: 1px solid blue;
+  }
+  .categories {
+    padding: 1rem 0;
+  }
+  .category {
+    padding: 0.25em 0.75em;
+    margin: 0 0.5em;
+    border-radius: 0.5rem;
+  }
+  .red, .blue, .orange, .green, .purple, .teal {
+    color: white;
+  }
+  .red {
+    background-color: #761f1f;
+  }
+  .blue {
+    background-color: #1f3076;
+  }
+  .orange {
+    background-color: #7e4f19;
+  }
+  .green {
+    background-color: #28761f;
+  }
+  .purple {
+    background-color: #6c1f76;
+  }
+  .teal {
+    background-color: #1f7276;
+  }
+</style>
