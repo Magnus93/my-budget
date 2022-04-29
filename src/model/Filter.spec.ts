547e2c40d498c11b971c3932bcebf64f7930bcc9
Magnus93 <magnus.ja.gustafsson@gmail.com>
2022-04-29 08:14:22 +0200

    Filter apply function works

--- src/model/Filter.spec.ts
+++ src/model/Filter.spec.ts
@@ -3,13 +3,20 @@ import { Transaction } from "./Transaction"
 
 describe("Filter", () => {
   let trans: Transaction[] = [
-    {category: "furniture", description: "", amount: 30, date: "2022-03-01"},
-    {category: "health", description: "", amount: 10, date: "2022-02-01"},
-    {category: "health", description: "", amount: 40, date: "2022-05-01"},
-    {description: "", amount: 20, date: "2022-06-01"},
-    {category: "misc", description: "", amount: 30, date: "2022-12-01"},
+    {category: "furniture", description: "IKEA", amount: 30, date: "2022-03-01"},
+    {category: "health", description: "Ica Maxi", amount: 10, date: "2022-02-01"},
+    {category: "health", description: "Willys Store", amount: 40, date: "2022-05-01"},
+    {description: "Ica Maxi", amount: 20, date: "2022-06-01"},
+    {category: "resturant", description: "Max Burgare", amount: 15, date: "2022-02-15"},
+    {category: "misc", description: "Il Forno", amount: 110, date: "2022-04-16"},
   ]
   it("Filter.apply", () => {
     expect(Filter.apply({ category: ["health", undefined] }, trans)).toEqual([trans[1], trans[2], trans[3]])
+    expect(Filter.apply({ description: "max" }, trans)).toEqual([trans[1], trans[3], trans[4]])
+    expect(Filter.apply({ description: /(willys|maxi)/i }, trans)).toEqual([trans[1], trans[2], trans[3]])
+    expect(Filter.apply({ amount: {$gte: 15} }, trans)).toEqual([trans[0], trans[2], trans[3], trans[4], trans[5]])
+    expect(Filter.apply({ amount: {$lte: 30} }, trans)).toEqual([trans[0], trans[1], trans[3], trans[4]])
+    expect(Filter.apply({ date: {$gte: "2022-03-01"} }, trans)).toEqual([trans[0], trans[2], trans[3], trans[5]])
+    expect(Filter.apply({ date: {$lte: "2022-03-01"} }, trans)).toEqual([trans[0], trans[1], trans[4]])
   })
 })
--- src/model/Filter.ts
+++ src/model/Filter.ts
@@ -15,6 +15,20 @@ export namespace Filter {
       let keep = true
       if (filter.category && filter.category.length > 0)
         keep &&= filter.category.includes(t.category)
+      if (keep && typeof filter.description == "string" && filter.description != "") 
+        keep &&= RegExp(filter.description, "i").test(t.description)
+      if(keep && filter.description instanceof RegExp) {
+        filter.description.lastIndex = 0
+        keep &&= filter.description.test(t.description)
+      }
+      if (keep && filter.amount && typeof filter.amount.$gte == "number")
+        keep &&= filter.amount.$gte <= t.amount
+      if (keep && filter.amount && typeof filter.amount.$lte == "number")
+        keep &&= filter.amount.$lte >= t.amount
+      if (keep && filter.date && Date.is(filter.date?.$gte))
+        keep &&= filter.date.$gte <= t.date
+      if (keep && filter.date && Date.is(filter.date?.$lte))
+        keep &&= filter.date.$lte >= t.date
       return keep
         /* return (!((filter.category?.length ?? 0) > 0) || filter.category.includes(t.category)) &&
       ((!(typeof filter.description == "string" && filter.description != "")) || RegExp(filter.description, "i").test(t.description)) &&
